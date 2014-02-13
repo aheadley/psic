9264d09a822e69ea5c39e7e0a3cb5111f0aa6890
aheadley <aheadley@waysaboutstuff.com>
2014-02-13 14:55:31 -0500

    fix minor errors

--- gsiz.py
+++ gsiz.py
@@ -2,6 +2,7 @@
 
 import zlib
 import struct
+import os
 
 ZLIB_DEFAULT_LEVEL  = max(1, min(9, int(os.environ.get('ZLIB_DEFAULT_LEVEL', '1'))))
 ZLIB_WINDOW_SIZE = -15
@@ -10,9 +11,9 @@ class GczError(Exception): pass
 
 class GczHeader(object):
     MAGIC_COOKIE        = 0xB10BC001;
-    HEADER_STRUCT       = struct.Struct('>IIQQII')
-    POINTERS_STRUCT_FMT = '>%dQ'
-    HASHES_STRUCT_FMT   = '>%dI'
+    HEADER_STRUCT       = struct.Struct('IIQQII')
+    POINTERS_STRUCT_FMT = '%dQ'
+    HASHES_STRUCT_FMT   = '%dI'
 
     def __init__(self, handle=None):
         if handle is not None:
@@ -34,19 +35,19 @@ class GczHeader(object):
         return struct.calcsize(self.POINTERS_STRUCT_FMT % len(self.block_pointers))
 
     @property
-    def hashes_size(self):
-        return struct.calcsize(self.HASHES_STRUCT_FMT % len(self.hashes))
+    def block_hashes_size(self):
+        return struct.calcsize(self.HASHES_STRUCT_FMT % len(self.block_hashes))
 
     @property
     def full_size(self):
-        return self.size + self.block_pointers_size + self.hashes_size
+        return self.size + self.block_pointers_size + self.block_hashes_size
 
     def _load_pointers_and_hashes(self, handle):
         pointers_struct = struct.Struct(self.POINTERS_STRUCT_FMT % self.num_blocks)
         handle.seek(self.size)
         self.block_pointers = list(pointers_struct.unpack(handle.read(pointers_struct.size)))
-        handles_struct = struct.Struct(self.HASHES_STRUCT_FMT % self.num_blocks)
-        self.hashes = list(hashes_struct.unpack(handle.read(hashes_struct.size)))
+        hashes_struct = struct.Struct(self.HASHES_STRUCT_FMT % self.num_blocks)
+        self.block_hashes = list(hashes_struct.unpack(handle.read(hashes_struct.size)))
 
 class GczFile(object):
     def __init__(self, handle):
@@ -54,7 +55,7 @@ class GczFile(object):
         self.header = GczHeader(handle)
 
     def __len__(self):
-        return len(self.handle.block_pointers)
+        return len(self.header.block_pointers)
 
     def get_block_compressed_size(self, block_num):
         start_pos = self.header.block_pointers[block_num]
@@ -63,7 +64,7 @@ class GczFile(object):
         elif block_num == header.num_blocks - 1:
             return self.header.compressed_data_size - start_pos
         else:
-            raise new GczError('Illegal block number: %d' % block_num)
+            raise GczError('Illegal block number: %d' % block_num)
 
     def compute_block_hash(self, block_data):
         return zlib.adler32(block_data)
@@ -86,10 +87,10 @@ class GczFile(object):
         self._handle.seek(block_offset)
         block_data = self._handle.read(compressed_block_size)
 
-        block_hash = self.compute_block_hash(block_hash)
-        if block_hash != self.handle.block_hashes[block_num]:
+        block_hash = self.compute_block_hash(block_data)
+        if block_hash != self.header.block_hashes[block_num]:
             raise GczError('Hash of block %d is wrong: %08x != %08x' % \
-                (block_num, block_hash, self.handle.block_hashes[block_num]))
+                (block_num, block_hash, self.header.block_hashes[block_num]))
 
         if not compressed:
             return block_data
